eaf9615eddd71d4a9ee91329df5b939acbce3dac
liamogreene <greenel3@tcd.ie>
2017-11-27 17:41:22 +0000

    Tidied Code

--- GithubUserData.java
+++ GithubUserData.java
@@ -1,11 +1,7 @@
 import java.io.BufferedReader;
-
-// Get API Login, then has access to current user, use data to present 
-
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class GithubUserData {
@@ -19,31 +15,25 @@ public class GithubUserData {
 		
 		System.out.println(url);
 
-		//Create HttpURLConnection 
+		//Connect to retrieve data
 		HttpURLConnection httpcon = (HttpURLConnection) new URL(url).openConnection();
 		httpcon.addRequestProperty("User-Agent", "Mozilla/5.0");
 		BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream()));
 
-		//Read line by line
-		StringBuilder responseSB = new StringBuilder();
+		//Read in data
+		StringBuilder jconData = new StringBuilder();
 		String line;
 		while ( ( line = in.readLine() ) != null) {
-			responseSB.append("\n" + line);
+			jconData.append("\n" + line);
 		}
 		in.close();
 		
-		String data = responseSB.toString();
+		//Display data
+		String data = jconData.toString();
 		String[] data2;
 		data2 = data.split(",");
 		for (int i = 0;i<data2.length; i++) {
-			if(data2[i].contains("_url")) {
-				System.out.println("-----");
-			}
-			else {
 				System.out.println(data2[i]);
-			}
 		}
-		
 	}
 }
-	
